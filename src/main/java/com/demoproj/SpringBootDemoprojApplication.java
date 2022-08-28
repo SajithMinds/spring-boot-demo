bc36a3730229740264fcb4e0175437181be61e64
Sajith <Sajith.S@gmail.com>
2022-08-29 00:14:22 +0530

    First commit

---
+++ src/main/java/com/demoproj/SpringBootDemoprojApplication.java
@@ -0,0 +1,19 @@
+package com.demoproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SpringBootDemoprojApplication {
+
+	@GetMapping("/hi")
+	public String home() {
+		return "Welcome to Azure platform";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoprojApplication.class, args);
+	}
+
+}
